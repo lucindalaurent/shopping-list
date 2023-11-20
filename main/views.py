8e40670dae79f3a7cfea61a04895b8cc1631d0a8
lucindalaurent <lucindalaurent21@gmail.com>
2023-11-20 14:39:28 +0700

    commit left update

--- main/views.py
+++ main/views.py
@@ -14,6 +14,7 @@ from django.views.decorators.csrf import csrf_exempt
 import datetime
 import json
 
+
 #menghubungkan template dan model, misah tampilan data
 # Create your views here.
 @login_required(login_url='/login')
